b42027cf3cdc7a8f392b156e7afbc270757b87f6
Spareribs <370835062@qq.com>
2018-03-09 19:26:42 +0800

    hotfix decisiontree code

--- Book_two/chapter3 DecisionTree/code/decisiontree.py
+++ Book_two/chapter3 DecisionTree/code/decisiontree.py
@@ -5,16 +5,16 @@
 # @Author  : Spareribs
 # @File    : decisiontree.py
 """
-
+import json
 import operator
 from math import log
 from collections import Counter
 
 
 def createDataSet():
-    """DateSet 基础数据集
-    Args: 无传入
-    Returns: 数据集，标签
+    """
+    生成基础数据集
+    :return: 数据集，标签列表
     """
     dataSet = [[1, 1, 'yes'],
                [1, 1, 'yes'],
@@ -29,54 +29,36 @@ def createDataSet():
 def calcShannonEnt(dataSet, methodSelect="one"):
     """
     计算给定数据集的香农熵
+    注意：默认dataSet中最后一列的值为结果值
     :param dataSet: 数据集
     :param methodSelect: 选择处理的方法
     :return: 每一组feature下的某个分类下，香农熵的信息期望
     """
 
-    ##########
-    # 方法一：#
-    ##########
+    #########
+    # 方法一 #
+    #########
     if methodSelect == "one":
-        # 计算dataSet数据集的量
-        numEntries = len(dataSet)
-        print(u"[Info]：dataSet的类型为:{0},dataSet的长度为:{1}".format(type(dataSet), numEntries))
-
-        # 计算分类标签label出现的次数
-        labelCounts = {}
-        for featVec in dataSet:
-            # 将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
+        numEntries = len(dataSet)  # 计算数据集的量
+        labelCounts = {}  # 计算分类标签label出现的次数，默认最后一列
+        for featVec in dataSet:  # 为所有可能分类创建字典
             currentLabel = featVec[-1]
-            # 为所有可能的分类创建字典，如果当前的键值不存在，则扩展字典并将当前键值加入字典。每个键值都记录了当前类别出现的次数。
             if currentLabel not in labelCounts.keys():
                 labelCounts[currentLabel] = 0
             labelCounts[currentLabel] += 1
-        print(u"[Info]：标签统计结果：{0}".format(labelCounts))
 
-        # 对于label标签的占比，求出label标签的香农熵
-        shannonEnt = 0.0
+        shannonEnt = 0.0  # 对于label标签的占比，求出label标签的香农熵
         for key in labelCounts:
-            # 使用所有类标签的发生频率计算类别出现的概率。
-            prob = float(labelCounts[key]) / numEntries
-            # 计算香农熵，以 2 为底求对数
-            shannonEnt -= prob * log(prob, 2)
-        print(u"[Info]：香农熵：{0}".format(shannonEnt))
+            prob = float(labelCounts[key]) / numEntries  # 计算所有标签的发生频率计算类别出现的概率
+            shannonEnt -= prob * log(prob, 2)  # 使用概率计算香农熵（以2为底求对数）
 
-    ##########
-    # 方法二：#
-    ##########
+    #########
+    # 方法二 #
+    #########
     else:
-        # 统计标签出现的次数
-        label_count = Counter(data[-1] for data in dataSet)
-        print(u"[Info]：结果统计字典：{0}".format(label_count))
-
-        # 计算概率
-        probs = [p[1] / len(dataSet) for p in label_count.items()]
-        print(u"[Info]：{0}".format(probs))
-
-        # 计算香农熵
-        shannonEnt = sum([-p * log(p, 2) for p in probs])
-        print(u"[Info]：{0}".format(shannonEnt))
+        labelCounts = Counter(data[-1] for data in dataSet)  # 统计标签出现的次数
+        probs = [p[1] / len(dataSet) for p in labelCounts.items()]  # 计算概率
+        shannonEnt = sum([-p * log(p, 2) for p in probs])  # 计算香农熵
 
     return shannonEnt
 
@@ -96,24 +78,18 @@ def splitDataSet(dataSet, index, value, methodSelect="one"):
     ##########
     if methodSelect == "one":
         retDataSet = []
-        for featVec in dataSet:
-            # index列为value的数据集【该数据集需要排除index列】
-            # 判断index列的值是否为value
-            if featVec[index] == value:
-                # chop out index used for splitting
-                # [:index]表示前index行，即若 index 为2，就是取 featVec 的前 index 行
-                reducedFeatVec = featVec[:index]
-                reducedFeatVec.extend(featVec[index + 1:])
-                # [index+1:]表示从跳过 index 的 index+1行，取接下来的数据
-                # 收集结果值 index列为value的行【该行需要排除index列】
-                retDataSet.append(reducedFeatVec)
-        print(u"[Info]：数据集为：{0}".format(retDataSet))
+        for featVec in dataSet:  # 循环获取每行的数据
+            if featVec[index] == value:  # 判断索引中的值与给定的目标值是否相等
+                reducedFeatVec = featVec[:index]  # 取索引前的所有数据
+                reducedFeatVec.extend(featVec[index + 1:])  # 取索引后的所有数据
+                retDataSet.append(reducedFeatVec)  # 将排除了索引值的结果存入新的retDataSet中
 
     ##########
     # 方法二：#
     ##########
     else:
         # retDataSet = [data for data in dataSet for i, v in enumerate(data) if i == axis and v == value]
+        retDataSet = ""
         pass
     return retDataSet
 
@@ -126,44 +102,30 @@ def chooseBestFeatureToSplit(dataSet, methodSelect="one"):
     :return: 最优的特征列
     """
 
-    ##########
-    # 方法一：#
-    ##########
+    #########
+    # 方法一 #
+    #########
     if methodSelect == "one":
-        # 求第一行有多少列的 Feature, 最后一列是label列嘛
-        numFeatures = len(dataSet[0]) - 1
-        # label的信息熵
-        baseEntropy = calcShannonEnt(dataSet)
-        # 最优的信息增益值, 和最优的Featurn编号
-        bestInfoGain, bestFeature = 0.0, -1
-        # iterate over all the features
-        for i in range(numFeatures):
-            # create a list of all the examples of this feature
-            # 获取每一个实例的第i+1个feature，组成list集合
-            featList = [example[i] for example in dataSet]
-            # get a set of unique values
-            # 获取剔重后的集合，使用set对list数据进行去重
-            uniqueVals = set(featList)
-            # 创建一个临时的信息熵
-            newEntropy = 0.0
-            # 遍历某一列的value集合，计算该列的信息熵
-            # 遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集，计算数据集的新熵值，并对所有唯一特征值得到的熵求和。
-            for value in uniqueVals:
-                subDataSet = splitDataSet(dataSet, i, value)
-                prob = len(subDataSet) / float(len(dataSet))
-                newEntropy += prob * calcShannonEnt(subDataSet)
-            # gain[信息增益]: 划分数据集前后的信息变化， 获取信息熵最大的值
-            # 信息增益是熵的减少或者是数据无序度的减少。最后，比较所有特征中的信息增益，返回最好特征划分的索引值。
-            infoGain = baseEntropy - newEntropy
-            print('infoGain=', infoGain, 'bestFeature=', i, baseEntropy, newEntropy)
-            if (infoGain > bestInfoGain):
+        numFeatures = len(dataSet[0]) - 1  # 求dataSet出特征的数量
+        baseEntropy = calcShannonEnt(dataSet)  # 计算dataSet的信息熵（香农熵）
+        bestInfoGain, bestFeature = 0.0, -1  # 设置最优的信息增益值, 和最优的特征编号
+        for i in range(numFeatures):  # 通过循环迭代所有的特征
+            featList = [example[i] for example in dataSet]  # 获取当前列的所有特征值，组成一个List
+            uniqueVals = set(featList)  # 特征值去重
+            newEntropy = 0.0  # 创建一个临时的信息熵
+            for value in uniqueVals:  # 遍历去重后的特征
+                subDataSet = splitDataSet(dataSet, i, value)  # 重新划分数据集
+                prob = len(subDataSet) / float(len(dataSet))  # 计算新数据集占原数据集的比例
+                newEntropy += prob * calcShannonEnt(subDataSet)  # 将所有的数据集计算的香农熵求和，得到信息熵
+            infoGain = baseEntropy - newEntropy  # 差值越大，说明newEntropy越小，没那么混乱
+            if infoGain > bestInfoGain:  # 比较所有特征中的信息增益，返回最好特征划分的索引值
                 bestInfoGain = infoGain
                 bestFeature = i
         return bestFeature
     else:
-        ##########
-        # 方法二：#
-        ##########
+        #########
+        # 方法二 #
+        #########
         # 计算初始香农熵
         base_entropy = calcShannonEnt(dataSet)
         best_info_gain = 0
@@ -185,109 +147,73 @@ def chooseBestFeatureToSplit(dataSet, methodSelect="one"):
         return best_feature
 
 
-def majorityCnt(classList):
-    """majorityCnt(选择出现次数最多的一个结果)
-
-    Args:
-        classList label列的集合
-    Returns:
-        bestFeature 最优的特征列
+def majorityCnt(classList, methodSelect="one"):
     """
-    # -----------majorityCnt的第一种方式 start------------------------------------
-    classCount = {}
-    for vote in classList:
-        if vote not in classCount.keys():
-            classCount[vote] = 0
-        classCount[vote] += 1
-    # 倒叙排列classCount得到一个字典集合，然后取出第一个就是结果（yes/no），即出现次数最多的结果
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
-    # print 'sortedClassCount:', sortedClassCount
-    return sortedClassCount[0][0]
-    # -----------majorityCnt的第一种方式 end------------------------------------
 
-    # # -----------majorityCnt的第二种方式 start------------------------------------
-    # major_label = Counter(classList).most_common(1)[0]
-    # return major_label
-    # # -----------majorityCnt的第二种方式 end------------------------------------
+    :param classList: 分类标签的列表
+    :return: 返回出现次数醉倒的分类名称
+    """
+
+    #########
+    # 方法一 #
+    #########
+    if methodSelect == "one":
+        classCount = {}
+        for vote in classList:
+            if vote not in classCount.keys():
+                classCount[vote] = 0
+            classCount[vote] += 1
+        # 倒叙排列classCount得到一个字典集合，然后取出第一个就是结果（yes/no），即出现次数最多的结果
+        sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+        # print 'sortedClassCount:', sortedClassCount
+        return sortedClassCount[0][0]
+    #########
+    # 方法二 #
+    #########
+    else:
+        major_label = Counter(classList).most_common(1)[0]
+        return major_label
 
 
 def createTree(dataSet, labels):
     """
-
-    :param dataSet:
-    :param labels:
+    创建树的函数代码
+    :param dataSet: 数据集
+    :param labels: 标签列表
     :return:
     """
-    classList = [example[-1] for example in dataSet]
-    # 如果数据集的最后一列的第一个值出现的次数=整个集合的数量，也就说只有一个类别，就只直接返回结果就行
-    # 第一个停止条件：所有的类标签完全相同，则直接返回该类标签。
-    # count() 函数是统计括号中的值在list中出现的次数
-    if classList.count(classList[0]) == len(classList):
+    classList = [example[-1] for example in dataSet]  # 数据集的所有类标签
+    if classList.count(classList[0]) == len(classList):  # 类别完全相同则停止继续划分
         return classList[0]
-    # 如果数据集只有1列，那么最初出现label次数最多的一类，作为结果
-    # 第二个停止条件：使用完了所有特征，仍然不能将数据集划分成仅包含唯一类别的分组。
-    if len(dataSet[0]) == 1:
+    if len(dataSet[0]) == 1:  # 遍历完所有特征时，返回出现次数最多的
         return majorityCnt(classList)
-
-    # 选择最优的列，得到最优列对应的label含义
-    bestFeat = chooseBestFeatureToSplit(dataSet)
-    # 获取label的名称
+    bestFeat = chooseBestFeatureToSplit(dataSet)  # 存储最好特征的变量
     bestFeatLabel = labels[bestFeat]
-    # 初始化myTree
     myTree = {bestFeatLabel: {}}
-    # 注：labels列表是可变对象，在PYTHON函数中作为参数时传址引用，能够被全局修改
-    # 所以这行代码导致函数外的同名变量被删除了元素，造成例句无法执行，提示'no surfacing' is not in list
-    del (labels[bestFeat])
-    # 取出最优列，然后它的branch做分类
-    featValues = [example[bestFeat] for example in dataSet]
+    del (labels[bestFeat])  # 当特征被加入到myTree中以后，删除该特征
+    featValues = [example[bestFeat] for example in dataSet]  # 得到列表包含的所有属性值
     uniqueVals = set(featValues)
     for value in uniqueVals:
-        # 求出剩余的标签label
         subLabels = labels[:]
-        # 遍历当前选择特征包含的所有属性值，在每个数据集划分上递归调用函数createTree()
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
-        # print 'myTree', value, myTree
     return myTree
-
-
-def classify(inputTree, featLabels, testVec):
-    """
-
-    :param inputTree:
-    :param featLabels:
-    :param testVec:
-    :return:
-    """
-    """classify(给输入的节点，进行分类)
-
-    Args:
-        inputTree  决策树模型
-        featLabels Feature标签对应的名称
-        testVec    测试输入的数据
-    Returns:
-        classLabel 分类的结果值，需要映射label才能知道名称
-    """
-    # 获取tree的根节点对于的key值
-    firstStr = inputTree.keys()[0]
-    # 通过key得到根节点对应的value
-    secondDict = inputTree[firstStr]
-    # 判断根节点名称获取根节点在label中的先后顺序，这样就知道输入的testVec怎么开始对照树来做分类
-    featIndex = featLabels.index(firstStr)
-    # 测试数据，找到根节点对应的label位置，也就知道从输入的数据的第几位来开始分类
-    key = testVec[featIndex]
-    valueOfFeat = secondDict[key]
-    print('+++', firstStr, 'xxx', secondDict, '---', key, '>>>', valueOfFeat)
-    # 判断分枝是否结束: 判断valueOfFeat是否是dict类型
-    if isinstance(valueOfFeat, dict):
-        classLabel = classify(valueOfFeat, featLabels, testVec)
-    else:
-        classLabel = valueOfFeat
-    return classLabel
 
 
 if __name__ == "__main__":
     dataSet, labels = createDataSet()
-    shannonEnt = calcShannonEnt(dataSet)
-    splitDataSet(dataSet, 2, "yes")
-    print("*"*100)
-    print(chooseBestFeatureToSplit(dataSet))
+    print(u"[Info]：当前创建出来的数据集为{0}, 结果标签为{1}".format(dataSet, labels))
+    ################################
+    # 这部分是用于测试各部分函数的例子 #
+    ################################
+    # # calcShannonEnt 函数测试
+    # shannonEnt = calcShannonEnt(dataSet)
+    # print(u"[Info]：数据集 {0} 的香农熵为 {1} .".format(dataSet, shannonEnt))
+    # # splitDataSet 函数测试
+    # retDataSet = splitDataSet(dataSet, 1, 1)
+    # print(u"[Info]: 数据集 {0} 按照索引1的值为1的方式划分，得到新的数据集 {1}".format(dataSet, retDataSet))
+    # # chooseBestFeatureToSplit 函数测试
+    # bestFeature = chooseBestFeatureToSplit(dataSet)
+    # print(u"[Info]: 通过最好的特征选择，索引为 {0} 的特征最好".format(bestFeature))
+
+    myTree = createTree(dataSet, labels)
+    print(u"[Info]：决策树为：\n{2}".format(dataSet, labels, json.dumps(myTree, indent=4, separators=(',', ': '))))
